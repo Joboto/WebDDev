312f64c46a3ae1481a9065735ab56a40e5c969a7
Joboto <joeisamonkey@gmail.com>
2015-02-10 13:01:54 +0000

    A start
    Changing layout and adding content. Leaving 'container', 'header',
    'main' (inc. navigation and maincontainer) and 'footer'.
    created nav links but not linking to anything. embeded wtf youtube vid.
    Put in some text w/ links in text to 'about' and 'courses'.
    Still need login/register box top right.

--- movement/index.php
+++ movement/index.php
@@ -8,26 +8,38 @@
 <body>
 <div id="container">
 	<div id="header">
-		<h4 style="float:left;padding-left:0.625em">Welcome to Movement</h4>
-		<h4 style="float:right;padding-right:0.625em">info@movement.com</h4>
-		<div style="clear:both"></div>
-		<div id="navigation"></div>
-		<div id="navright"></div>
-		<!--<div id="baighdiv"></div>-->
-		<!--<div id="weeImagesLeft"></div>-->
-		<!--<div id="weeImagesRight"></div>-->
+		<h1>MOVEMENT</h1>
+		<h4>Start yours...</h4>
 	</div>
 	<div id="Main">
+		<div id="navigation">
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Courses</a></li>
+				<li><a href="#">Equipment</a></li>
+				<li><a href="#">Events</a></li>
+				<li><a href="#">Articles</a></li>
+				<li><a href="#">Links</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
 		<div id="Maincontainer">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/Su0vLwyQTTI" frameborder="0" allowfullscreen></iframe>
+			<!--<iframe width="420" height="315" src="https://www.youtube.com/embed/fdWDvH8AITU" frameborder="0" allowfullscreen></iframe>-->
+			<p>Here you will find everything you need to get moving! From parkour to gymnastic strength to the subtleties of the Feldenkrais method.
+			Check out our <a href="#">courses</a> page to see what's available and get started! <br>For an overview of our philosophy and approach, 
+			have a look at our <a href="#about">bio</a>.
+			</p>
 		</div>
 	</div>
+	<div style="clear:both"></div>
 	<footer id="footer">
 		<div id="flinks">
 			<a href="http://www.facebook.co.uk" target="_blank"><img src="images/facebook.png" alt="facebook.png" title="Go to Facebook" /></a>
 			<a href="http://www.twitter.co.uk" target="_blank"><img src="images/twitter.png" alt="twitter.png" title="Go to Twitter" /></a>
 		</div>
 		<div id="weelinks">
-			<h4 style="text-align:center">| Media | Careers | Corporate | Access |</h4>
+			<h4 style="text-align:center">| Disclaimer | Careers | Corporate | Access |</h4>
 		</div>
 		<br />
 	</footer>
