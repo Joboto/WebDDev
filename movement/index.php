f0f9446fe5305d6ee42439844ee29e9a5913da8e
Joboto <joeisamonkey@gmail.com>
2015-02-10 11:36:26 +0000

    Adding movement folders
    copying from wamp/www folder. Will have to copy back to run on wamp
    server later.

---
+++ movement/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Movement - just move...</title>
+	<link rel="stylesheet" href="css/structure.css" type="text/css" />
+	<link rel="stylesheet" href="css/navigation.css" type="text/css" />
+</head>
+<body>
+<div id="container">
+	<div id="header">
+		<h4 style="float:left;padding-left:0.625em">Welcome to Movement</h4>
+		<h4 style="float:right;padding-right:0.625em">info@movement.com</h4>
+		<div style="clear:both"></div>
+		<div id="navigation"></div>
+		<div id="navright"></div>
+		<!--<div id="baighdiv"></div>-->
+		<!--<div id="weeImagesLeft"></div>-->
+		<!--<div id="weeImagesRight"></div>-->
+	</div>
+	<div id="Main">
+		<div id="Maincontainer">
+		</div>
+	</div>
+	<footer id="footer">
+		<div id="flinks">
+			<a href="http://www.facebook.co.uk" target="_blank"><img src="images/facebook.png" alt="facebook.png" title="Go to Facebook" /></a>
+			<a href="http://www.twitter.co.uk" target="_blank"><img src="images/twitter.png" alt="twitter.png" title="Go to Twitter" /></a>
+		</div>
+		<div id="weelinks">
+			<h4 style="text-align:center">| Media | Careers | Corporate | Access |</h4>
+		</div>
+		<br />
+	</footer>
+</div>
+
+</body>
+</html>
+<!--<?php 
+ session_start();
+ if(isset($_SESSION['user'])){
+  echo "<script>window.location='member/member.php'</script>";
+ } else {
+  echo "<script>window.location='account/login.php'</script>";
+ }	
+?>-->
+
